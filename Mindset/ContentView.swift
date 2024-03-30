c51308480c20557f4b5339d9d66e58eeb33f5ffd
Patrick Ridd <patrickridd@gmail.com>
2024-03-30 12:32:30 -0600

    Initial Commit

---
+++ Mindset/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mindset
+//
+//  Created by patrick ridd on 3/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mindset/MindsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindsetApp.swift
+//  Mindset
+//
+//  Created by patrick ridd on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MindsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
