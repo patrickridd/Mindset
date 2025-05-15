966dadfc7a516117ca530351e91d2fd187b50ff1
Patrick Ridd <patrickridd@gmail.com>
2025-05-14 21:12:33 -0600

    Initial Commit

---
+++ Mindset/MindsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindsetApp.swift
+//  Mindset
+//
+//  Created by patrick ridd on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MindsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
