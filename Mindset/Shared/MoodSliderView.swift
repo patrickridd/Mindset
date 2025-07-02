3617044ed9fe7118af81e7fea2176440d87865a4
Patrick Ridd <patrickridd@gmail.com>
2025-07-02 13:38:04 -0600

    Added animation that signals to the user to select their mood.

--- Mindset/Shared/MoodSliderView.swift
+++ Mindset/Shared/MoodSliderView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct MoodSliderView: View {
 
+    @State private var animate = false
+    @State private var hasInteracted: Bool = false
     @Binding var moodValue: Double
 
     let emojiMap = [0: "😭", 1:"😞", 2: "🙁", 3: "😐", 4: "🙂", 5: "😀", 6: "🤩"]
@@ -28,6 +30,18 @@ struct MoodSliderView: View {
             Slider(value: $moodValue, in: 0...Double(emojiMap.count-1), step: 1)
                 .accentColor(.indigo)
                 .sensoryFeedback(.selection, trigger: moodValue)
+                .shadow(color: Color.orange.opacity(0.7), radius: animate ? 14 : 0)
+                .scaleEffect(animate ? 1.1 : 1)
+                .animation(animate ? .easeInOut(duration: 1).repeatForever(autoreverses: true) : .default,
+                    value: animate
+                )
+                .onAppear {
+                    animate = !hasInteracted
+                }
+                .onChange(of: moodValue) { _, _ in
+                    animate = false
+                    hasInteracted = true
+                }
         }
         .padding(.horizontal, 24)
     }
